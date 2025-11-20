e454035d856c5a050e5270cde0a60773776bb949
Evan Peepo <peepo@wisc.edu>
2025-11-20 15:32:20 -0600

    code update

--- HW12_Evan.R
+++ HW12_Evan.R
@@ -1,7 +1,10 @@
 library(readxl)
 library(tidyverse)
 
-bsb_data <- read_xlsx('/Users/evanpeepo/Documents/Classes/ZOO800 Quant methods/BSB_tagging_data.xlsx')
+
+# Objective 1 -------------------------------------------------------------
+
+bsb_data <- read_xlsx('BSB_tagging_data.xlsx')
 
 str(bsb_data) #date is in POSIXct
 
@@ -38,3 +41,48 @@ density <- dbeta(probability, shape1 = 10, shape2 = 21) #shape1 = # changed + 1,
 plot(probability, density, type = 'l')
 
 #B.
+conf_int <- c(qbeta(.025, 10, 21), qbeta(.975, 10, 21))
+conf_int
+
+# Objective 2 -------------------------------------------------------------
+#Make sex change binary variable
+bsb_capture_after_july <- bsb_capture_after_july %>% 
+  filter(month_at_recapture > 7, Sex_at_capture == "F") %>% 
+  mutate(
+    changed_sex_binary = if_else(Sex_at_capture == Sex_at_recapture, 0, 1)
+  )
+
+#Look at data
+hist(bsb_capture_after_july$Length_at_capture)
+
+ggplot(bsb_capture_after_july, aes(x = Length_at_capture, y = changed_sex)) +
+  geom_boxplot()
+
+#Make GLM
+length_glm <- glm(changed_sex_binary ~ Length_at_capture, family = 'binomial', data = bsb_capture_after_july)
+
+#A. 
+summary(length_glm) #p = .11
+coef(length_glm)
+
+#B. 
+#For every mm of length increase, log odds is expected to increase by .044. 
+
+#C. 
+
+predictors <- seq(min(bsb_capture_after_july$Length_at_capture), 
+                  max(bsb_capture_after_july$Length_at_capture), 
+                  length = 100
+                  )
+
+length_df <- data.frame(Length_at_capture = predictors)
+
+predicted_odds <- predict.glm(length_glm, newdata = length_df, type = 'response')
+
+length_and_odds_df <- data.frame(length = predictors, predicted_odds)
+
+ggplot(length_and_odds_df, aes(x = length, y = predicted_odds)) +
+  geom_line() +
+  theme_minimal(base_size = 15) +
+  ylab('Pred. Odds of Sex Change') +
+  xlab('Length (mm)')
